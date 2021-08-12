03437e32b7e33d129da3d0485336cc0f302eb385
Vitor Peixoto <vitorpeixoto14@hotmail.com>
2021-08-12 12:32:31 -0300

    updated scratch template

--- templates/scratch/src/index.tsx
+++ templates/scratch/src/index.tsx
@@ -1,22 +1,16 @@
 import React from "react";
-import { hydrate, render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./assets/styles/index.scss";
-import { renderDataToDOM } from "./helpers/helpers";
+import { open } from "@soltivo/theme-library";
 
-const open = process.env.NODE_ENV === "development" ? render : hydrate;
-const global = renderDataToDOM();
-
-const basename = window.location.hostname.includes("builder.mysoltivo")
-  ? `/themes/${process.env.THEME_ID}`
-  : "/";
-
-open(
-  <React.StrictMode>
-    <BrowserRouter basename={basename}>
-      <App global={global} />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+open((data, render, { basename }) => {
+  render(
+    <React.StrictMode>
+      <BrowserRouter basename={basename}>
+        <App global={data} />
+      </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+});
