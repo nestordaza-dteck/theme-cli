dd259fd83d02e5fed28f6649b6dc7a2b3652fff0
Benjamin Filiatrault <benjamin@soltivo.com>
2021-11-10 15:26:44 -0500

    typo fix

--- src/scripts/plugins/retriveThemeData.ts
+++ src/scripts/plugins/retriveThemeData.ts
@@ -6,26 +6,26 @@ import { createGlobalData } from "../helpers/createGlobalData";
  * @description it gets the data from sections, pages and global and creates the json data
  * inside theme's public folder project.
  */
-export class RetriveThemeData {
-  apply(compiler: Compiler) {
-    compiler.hooks.done.tap("RetriveThemeData", async (stats) => {
-      const { outputPath, assets } = stats.toJson();
+export class RetrieveThemeData {
+	apply(compiler: Compiler) {
+		compiler.hooks.done.tap("RetrieveThemeData", async (stats) => {
+			const { outputPath, assets } = stats.toJson();
 
-      // get all available sections from theme directory under:
-      // ${projectDir}/src/sections/${sectionName}/${sectionName}.data.ts|js
-      const sections = await getThemeSections({ assets, outputPath });
-      // create pages.data.json and retrive it.
-      const pages = await createPagesData({
-        assets,
-        outputPath,
-        sections,
-      });
-      // create validation, config, and global json files.
-      await createGlobalData({
-        assets,
-        outputPath,
-        pages: pages,
-      });
-    });
-  }
+			// get all available sections from theme directory under:
+			// ${projectDir}/src/sections/${sectionName}/${sectionName}.data.ts|js
+			const sections = await getThemeSections({ assets, outputPath });
+			// create pages.data.json and retrive it.
+			const pages = await createPagesData({
+				assets,
+				outputPath,
+				sections,
+			});
+			// create validation, config, and global json files.
+			await createGlobalData({
+				assets,
+				outputPath,
+				pages: pages,
+			});
+		});
+	}
 }
